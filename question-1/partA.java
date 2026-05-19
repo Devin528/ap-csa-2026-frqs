ddb3e6777d9cd6b1d219ce8070b9362e1b32798d
Devin528 <devinl87@nycstudents.net>
2026-05-19 10:10:22 -0400

    Update partA.java

--- question-1/partA.java
+++ question-1/partA.java
@@ -1,1 +1,18 @@
+public class Account {
+    
+    private String username;
 
+    public Account(String requestedName) {
+        String username = " ";
+        if (requestedName.isAvailable) {
+            String username = requestedName;
+        }
+        else {
+            int i = 1;
+            while(!requestedName.isAvailable) {
+                username = requestedName + i;
+                i++;
+            }
+        }
+    }
+}
