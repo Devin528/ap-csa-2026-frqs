b0e083b3ac08ec09b68762983ce65ed212f031b8
Devin528 <devinl87@nycstudents.net>
2026-05-21 09:48:23 -0400

    Update PartB.java
    unfinished

--- question-1/PartB.java
+++ question-1/PartB.java
@@ -1,1 +1,8 @@
-
+public String getShortenedName() {
+  shortenedName = "";
+  for (int i = 0; i < username.length(); i++) {
+    if (indexOf("-") = -1) {
+      return username;
+    else {
+      shortenedName = 
+      
