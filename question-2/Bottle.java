43a01cd3ae193076e5981222a357fa4f245d78f1
Devin528 <devinl87@nycstudents.net>
2026-05-22 10:07:58 -0400

    Update Bottle.java

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,4 +1,18 @@
 public class Bottle {
+  private double amt;
+
+  public bottle(double a){
+    double a = amt;
+  }
+
+  public double updateAmount(double removeAmt) {
+    double updatedAmt;
+    if(a-removeAmt >= a*0.25) {
+      updatedAmt = a-removeAmt;
+      return updatedAmt;
+    else{
+      return a;
+    
 
 
 
